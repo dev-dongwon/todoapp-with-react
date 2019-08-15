2b482f880729ef8c8f0fd2d86e5e91b012657dfa
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-15 16:11:05 +0900

    Feat : state 정의 후 form 기능 구현

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -4,12 +4,20 @@ import TodoItem from './TodoItem';
 class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
+
+    const todoList = todos.map(
+      (todo) => (
+        <TodoItem
+        {...todo}
+        onToggle={onToggle}
+        onRemove={onRemove}
+        key={todo.id}/>
+      )
+    );
     
     return (
       <div>
-        <TodoItem text="hi"/>
-        <TodoItem text="hello"/>
-        <TodoItem text="good-bye"/>
+        { todoList }
       </div>
     );
   }
